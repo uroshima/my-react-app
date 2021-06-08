0ed7fc7b75e950cbd45402942e087ee512b29626
urosh <urosh.stojkovikj@gmail.com>
2021-06-08 18:28:10 -0400

    adding an error

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React Ladies & Gentleman</h2>
-          {/* <button onClick={methodDoesNotExist}>Break the world</button> */}
+          <button onClick={methodDoesNotExist}>Break the world</button>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
